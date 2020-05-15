da2da97772300559cc505142aea5a6887a3dca4a
qjrincoywork <quirjohnincoy.work@gmail.com>
2020-05-15 11:44:48 +0800

    add comment

---
+++ src/Controller/Api/CommentsController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller\Api;
+
+use Cake\ORM\TableRegistry;
+use Firebase\JWT\JWT;
+
+class CommentsController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadModel('Posts');
+        $this->loadComponent('RequestHandler');
+    }
+    
+    public function add()
+    {
+        $comment = $this->Comments->newEntity();
+        $datum['success'] = false;
+        if ($this->request->is('post')) {
+            $request = JWT::decode($this->request->getData('token'), 
+                                   $this->request->getData('api_key'), ['HS256']);
+                                   
+            $commentData = get_object_vars($request->data);
+            $comment = $this->Comments->patchEntity($comment, $commentData);
+            
+            if(!$comment->getErrors()) {
+                if ($this->Comments->save($comment)) {
+                    $datum['success'] = true;
+                }
+            } else {
+                $errors = $this->formErrors($comment);
+                $datum['errors'] = $errors;
+            }
+            
+            return $this->jsonResponse($datum);
+        }
+    }
+}
